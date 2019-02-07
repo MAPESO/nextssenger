bdc676b2ca1abe4c67bc0c1bedee4f251b25ab64
Mark Pena <markdrew53@gmail.com>
2019-02-07 10:59:28 -0600

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Middleware
 const withSession = require('./lib/middleware/session');
 
+// Uitls
+const baseTemplate = require('./lib/utils/baseTemplate');
+
 const existingUser = req => {
-  const user = req.session.user;
+  const { user } = req.session;
   if (user) {
     return user;
   }
@@ -17,18 +20,28 @@ module.exports = withSession((req, res) => {
       image: { url }
     } = JSON.parse(user);
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(`
-    <header style="text-align: center; font-family: sans-serif;">
-      <img src=${url} alt=${displayName} width=${100} height=${100}>
-      <p>🎉Welcome back, <b>${displayName}</b>!!</p>
-    </header>
-    `);
+    res.end(
+      baseTemplate(
+        `<header style="text-align: center; font-family: sans-serif;">
+            <img style="border-radius: 50%;" src=${url} alt=${displayName} width=${100} height=${100}>
+            <p>🎉Welcome back, <b>${displayName}</b>!!</p>
+            <form action="/logout" method="post">
+                <button type="submit">Log Out</button>
+            </form>
+        </header>`
+      )
+    );
   } else {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(`
-    <header style="text-align: center; font-family: sans-serif;">
-        <p>💩 <b>You are not logged in.</b></p>
-        <p><a href="/auth/google">Log in with Google :)</a></p>
-    </header>`);
+    res.end(
+      baseTemplate(
+        `<header style="text-align: center; font-family: sans-serif;">
+            <p>💩 <b>You are not logged in</b></p>
+            <form action="/auth/google" method="post">
+                <button type="submit">Log in with Google :)</button>
+            </form>
+        </header>`
+      )
+    );
   }
 });

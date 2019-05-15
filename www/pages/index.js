3052baa8ea0bd56e71491fc52ecc483d6a7cfa80
Mark Pena <markdrew53@gmail.com>
2019-05-15 12:59:25 -0500

    monorepo

---
+++ www/pages/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import Cookies from 'cookies';
+
+class Index extends Component {
+    static async getInitialProps({ req, res }) {
+        const cookies = new Cookies(req, res, { keys: [process.env.COOKIE_KEY_1, process.env.COOKIE_KEY_2] });
+        const isCookie = cookies.get('from-user-goole', { signed: true });
+        if(Object.is(isCookie, undefined)) {
+            res.writeHead(301, { Location: '/login' });
+            res.end();
+        }
+        return { isCookie }
+    }
+    render () {
+        const { displayName, url } = JSON.parse(this.props.isCookie);
+        return (
+            <Layout>
+                <h1>Welcome to Nextssenger {displayName} ! </h1>
+            </Layout>
+        );
+    }
+}
+
+export default Index;

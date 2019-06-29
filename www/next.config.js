2fc168a99cbec025352009399f66858e5caded8a
Mark Pena <markdrew53@gmail.com>
2019-06-28 21:03:29 -0500

    Set-up env

--- www/next.config.js
+++ www/next.config.js
@@ -1,5 +1,12 @@
-const nextConfig = {
-    target: 'serverless'
-}
+const withCSS = require('@zeit/next-css');
+
+const nextConfig = withCSS({
+  target: 'serverless',
+  env: {
+    CHATKIT_TOKEN: process.env.CHATKIT_TOKEN,
+    CHATKIT_LOCATOR: process.env.CHATKIT_LOCATOR,
+    JWT_KEY: process.env.JWT_KEY
+  }
+});
 
 module.exports = nextConfig;

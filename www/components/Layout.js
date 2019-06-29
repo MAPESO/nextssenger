868a34be1424373eb88318ad6a12bf92335dd015
Mark Pena <markdrew53@gmail.com>
2019-06-28 21:04:33 -0500

    Preparing the layout

--- www/components/Layout.js
+++ www/components/Layout.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
 
-function Layout(props) {
-    const { children } = props;
-    return (
-        <>
-          {children}
-        </>
-    )
+import '../styles/layout.css';
+
+function Layout(props) {
+  const { children } = props;
+  return (
+    <div className="App">
+      <style global jsx>{`
+        html {
+          box-sizing: border-box;
+        }
+        *,
+        *::before,
+        *::after {
+          box-sizing: inherit;
+          margin: 0;
+          padding: 0;
+        }
+
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+      `}</style>
+      <Head>
+        <title>chat</title>
+      </Head>
+      {children}
+    </div>
+  );
 }
 
-export default Layout;
+Layout.propTypes = {
+  children: PropTypes.any
+};
+
+export { Layout };

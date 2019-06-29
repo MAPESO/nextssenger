6745ceef871523224fd7f1b2f7e2011b52f442c6
Mark Pena <markdrew53@gmail.com>
2019-06-28 21:08:48 -0500

    Added login page

--- www/pages/login.js
+++ www/pages/login.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Layout from '../components/Layout';
 
 function Login() {
-    return (
-        <Layout>
-            <a href="/auth">SIG IN WITH GOOGLE</a>
-        </Layout>
-    )
+  return (
+    <div>
+      <a href="/auth">SIG IN WITH GOOGLE</a>
+    </div>
+  );
 }
 
 export default Login;

c4fdfc2a3460fb829343dd866dbf0d46b386968d
markoz <markdrew53@gmail.com>
2019-09-04 13:08:28 -0500

    Added Link component

--- www/pages/login.js
+++ www/pages/login.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Login() {
   return (
     <div>
-      <a href="/auth">SIG IN WITH GOOGLE</a>
+      <Link href="/auth">SIG IN WITH GOOGLE</Link>
     </div>
   );
 }

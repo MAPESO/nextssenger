bbb0b185113507cdd43ef55cf32277af6cf2f202
markoz <markdrew53@gmail.com>
2019-09-29 12:45:19 -0500

    Added Link component

--- www/components/Secreen.js
+++ www/components/Secreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 import '../styles/chat-screen.css';
@@ -7,7 +8,13 @@ function Screen(props) {
   const { children } = props;
   return (
     <section className="chat-screen">
-      <header className="chat-header"></header>
+      <header className="chat-header">
+        <div style={{ textAlign: 'center' }}>
+          <Link href="/logout">
+            <a>LOG OUT</a>
+          </Link>
+        </div>
+      </header>
       {children}
     </section>
   );
